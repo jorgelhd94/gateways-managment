1ad9bd053dc8882e8c873958e2fe855bfab14920
Jorgito <jorgelhd94@gmail.com>
2022-03-09 15:26:23 -0500

    add static Login Page

---
+++ src/Components/Auth/LoginSide/LoginSide.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const LoginSide = () => {
+  return (
+    <div className='absolute w-screen h-screen flex'>
+      <div className='hidden lg:block w-5/12 h-full'>
+        <img
+          src='https://images.unsplash.com/photo-1500672860114-9e913f298978?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=1049&amp;q=80'
+          className='w-full h-full object-cover'
+        />
+      </div>
+    </div>
+  );
+};
+
+export default LoginSide;
