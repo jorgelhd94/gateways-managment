fd40391c2225c7478045745c11e6962e6bcdb695
Jorgito <jorgelhd94@gmail.com>
2022-03-09 18:16:17 -0500

    add style to form inputs

--- src/Components/Auth/LoginForm/LoginForm.jsx
+++ src/Components/Auth/LoginForm/LoginForm.jsx
@@ -1,37 +1,47 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope, faLock } from '@fortawesome/free-solid-svg-icons';
 import { faGoogle } from '@fortawesome/free-brands-svg-icons';
 
 const LoginForm = () => {
+  const handleOnSubmit = (e) => {
+      e.preventDefault();
+  };
+
   return (
-    <form>
+    <form onSubmit={handleOnSubmit}>
       <div className='mt-12'>
+        
         <div className='my-6'>
-          <div className=''>
-            <div className=''>
-              <input
-                type='email'
-                name='email'
-                className='w-full border border-gray-300 rounded-sm px-4 py-3 outline-none transition-colors duration-150 ease-in-out focus:border-blue-400'
-                placeholder='Your email address'
-                value=''
-              />
-            </div>
+          <div className='flex relative '>
+            <span className='rounded-l-md inline-flex  items-center px-3 border-t bg-white border-l border-b  border-gray-300 text-gray-500 shadow-sm text-sm'>
+              <FontAwesomeIcon icon={faEnvelope} />
+            </span>
+            <input
+              type='text'
+              id='email-with-icon'
+              className=' rounded-r-lg flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent'
+              name='email'
+              placeholder='Your email'
+            />
           </div>
         </div>
+
         <div className='my-6'>
-          <div className=''>
-            <div className=''>
-              <input
-                type='password'
-                name='password'
-                className='w-full border border-gray-300 rounded-sm px-4 py-3 outline-none transition-colors duration-150 ease-in-out focus:border-blue-400'
-                placeholder='Your password'
-                value=''
-              />
-            </div>
+          <div className='flex relative '>
+            <span className='rounded-l-md inline-flex  items-center px-3 border-t bg-white border-l border-b  border-gray-300 text-gray-500 shadow-sm text-sm'>
+              <FontAwesomeIcon icon={faLock} />
+            </span>
+            <input
+              type='text'
+              id='password-with-icon'
+              className=' rounded-r-lg flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent'
+              name='password'
+              placeholder='Your password'
+            />
           </div>
         </div>
+        
         <div className='my-6'>
           <button
             className='inline-block rounded-sm font-medium border border-solid cursor-pointer text-center text-base py-3 px-6 text-white bg-blue-400 border-blue-400 hover:bg-blue-600 hover:border-blue-600 w-full'
--- src/Components/Auth/LoginSide/LoginSide.jsx
+++ src/Components/Auth/LoginSide/LoginSide.jsx
@@ -6,7 +6,7 @@ const LoginSide = () => {
     <div className='absolute w-screen h-screen flex'>
       <div className='hidden lg:block w-5/12 h-full'>
         <img
-          src='https://images.unsplash.com/photo-1500672860114-9e913f298978?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=1049&amp;q=80'
+          src='src/assets/img/bg-login.jpg'
           className='w-full h-full object-cover'
         />
       </div>
