e8400ab6c7fab2c0d41ab1576a600df0b6c6bd3a
Jorgito <jorgelhd94@gmail.com>
2022-03-10 13:00:43 -0500

    fixing style error

--- src/Components/Auth/LoginSide/LoginSide.jsx
+++ src/Components/Auth/LoginSide/LoginSide.jsx
@@ -10,13 +10,16 @@ const LoginSide = () => {
   };
 
   return (
-    <div className="absolute w-screen h-screen flex">
-      <div className="hidden lg:block w-5/12 h-full">
+    <div className='flex min-h-screen'>
+      <div className="hidden lg:block w-5/12">
         <img src="src/assets/img/bg-login.jpg" className="w-full h-full object-cover" />
       </div>
+
       <div className="w-5/6 sm:w-1/2 mx-auto text-center lg:px-8 xl:px-40 mt-16">
-        <div className="mt-10">
-          <h2 className="text-3xl font-bold text-gray-800">{isLogin ? 'Welcome' : 'Register'} to GMS!!</h2>
+        <div className="mt-4">
+          <h2 className="text-3xl font-bold text-gray-800">
+            {isLogin ? 'Welcome' : 'Register'} to GMS!!
+          </h2>
           <p className="mt-3 text-gray-800">
             {isLogin ? 'Are you new?' : 'Do you have an account?'}{' '}
             <a href="#" className="text-blue-400" onClick={toogleLogin}>
@@ -27,7 +30,7 @@ const LoginSide = () => {
 
         {isLogin ? <LoginForm /> : <RegisterForm />}
 
-        <p className="text-sm mt-12 text-center">
+        <p className="text-sm my-12 text-center">
           Created by{' '}
           <a href="https://github.com/jorgelhd94" target="_blank" rel="noreferrer">
             JCode Studio
