3627cd63a156d39ca5d19c3ea62efb55291a0778
Jorgito <jorgelhd94@gmail.com>
2022-03-10 09:11:17 -0500

    Revert "add state to change betwen login and signup"
    This reverts commit 46cc679dee360013372314acc490552cc2017524.

--- src/Components/Auth/LoginForm/LoginForm.jsx
+++ src/Components/Auth/LoginForm/LoginForm.jsx
@@ -5,59 +5,61 @@ import { faGoogle } from '@fortawesome/free-brands-svg-icons';
 
 const LoginForm = () => {
   const handleOnSubmit = (e) => {
-    e.preventDefault();
+      e.preventDefault();
   };
 
   return (
     <form onSubmit={handleOnSubmit}>
-      <div className="mt-12">
-        <div className="my-6">
-          <div className="flex relative ">
-            <span className="rounded-l-md inline-flex  items-center px-3 border-t bg-white border-l border-b  border-gray-300 text-gray-500 shadow-sm text-sm">
+      <div className='mt-12'>
+        
+        <div className='my-6'>
+          <div className='flex relative '>
+            <span className='rounded-l-md inline-flex  items-center px-3 border-t bg-white border-l border-b  border-gray-300 text-gray-500 shadow-sm text-sm'>
               <FontAwesomeIcon icon={faEnvelope} />
             </span>
             <input
-              type="text"
-              id="email-with-icon"
-              className=" rounded-r-lg flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent"
-              name="email"
-              placeholder="Your email"
+              type='text'
+              id='email-with-icon'
+              className=' rounded-r-lg flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent'
+              name='email'
+              placeholder='Your email'
             />
           </div>
         </div>
 
-        <div className="my-6">
-          <div className="flex relative ">
-            <span className="rounded-l-md inline-flex  items-center px-3 border-t bg-white border-l border-b  border-gray-300 text-gray-500 shadow-sm text-sm">
+        <div className='my-6'>
+          <div className='flex relative '>
+            <span className='rounded-l-md inline-flex  items-center px-3 border-t bg-white border-l border-b  border-gray-300 text-gray-500 shadow-sm text-sm'>
               <FontAwesomeIcon icon={faLock} />
             </span>
             <input
-              type="text"
-              id="password-with-icon"
-              className=" rounded-r-lg flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent"
-              name="password"
-              placeholder="Your password"
+              type='text'
+              id='password-with-icon'
+              className=' rounded-r-lg flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent'
+              name='password'
+              placeholder='Your password'
             />
           </div>
         </div>
-
-        <div className="my-6">
+        
+        <div className='my-6'>
           <button
-            className="inline-block rounded-sm font-medium border border-solid cursor-pointer text-center text-base py-3 px-6 text-white bg-blue-400 border-blue-400 hover:bg-blue-600 hover:border-blue-600 w-full"
-            type="submit">
+            className='inline-block rounded-sm font-medium border border-solid cursor-pointer text-center text-base py-3 px-6 text-white bg-blue-400 border-blue-400 hover:bg-blue-600 hover:border-blue-600 w-full'
+            type='submit'
+          >
             Log In
           </button>
         </div>
-        <div className="text-right">
-          <a href="#" className="text-blue-400">
+        <div className='text-right'>
+          <a href='#' className='text-blue-400'>
             Forgot your password?
           </a>
         </div>
-        <div className="flex flex-col mt-4 justify-items-center">
-          <p className="text-gray-800 mb-4">Or</p>
-          <div className="flex justify-center">
-            <button className="flex items-center px-6 py-2 transition ease-in duration-200 uppercase rounded-full hover:bg-gray-800 hover:text-white border-2 border-gray-900 focus:outline-none">
-              <FontAwesomeIcon icon={faGoogle} className="mr-4" />
+        <div className='flex flex-col mt-4 justify-items-center'>
+          <p className='text-gray-800 mb-4'>Or</p>
+          <div className='flex justify-center'>
+            <button className='flex items-center px-6 py-2 transition ease-in duration-200 uppercase rounded-full hover:bg-gray-800 hover:text-white border-2 border-gray-900 focus:outline-none'>
+              <FontAwesomeIcon icon={faGoogle} className='mr-4' />
               Sign in with Google
             </button>
           </div>
--- src/Components/Auth/LoginSide/LoginSide.jsx
+++ src/Components/Auth/LoginSide/LoginSide.jsx
@@ -1,37 +1,30 @@
-import React, { useState } from 'react';
+import React from 'react';
 import LoginForm from '../LoginForm/LoginForm';
 
 const LoginSide = () => {
-  const [isLogin, setIsLogin] = useState(true);
-
-  const toogleLogin = () => {
-    setIsLogin(!isLogin);
-  };
-
   return (
-    <div className="absolute w-screen h-screen flex">
-      <div className="hidden lg:block w-5/12 h-full">
-        <img src="src/assets/img/bg-login.jpg" className="w-full h-full object-cover" />
+    <div className='absolute w-screen h-screen flex'>
+      <div className='hidden lg:block w-5/12 h-full'>
+        <img
+          src='src/assets/img/bg-login.jpg'
+          className='w-full h-full object-cover'
+        />
       </div>
-      <div className="w-5/6 sm:w-1/2 mx-auto text-center lg:px-8 xl:px-40 mt-16">
-        <div className="mt-10">
-          <h2 className="text-3xl font-bold text-gray-800">
-            {isLogin ? 'Welcome' : 'Sign Up'} to GMS!!
-          </h2>
+      <div className='w-5/6 sm:w-1/2 mx-auto text-center lg:px-8 xl:px-40 mt-16'>
+        <div className='mt-10'>
+          <h2 className='text-3xl font-bold text-gray-800'>Welcome to GMS!!</h2>
+          <p className='mt-3 text-gray-800'>
+            Are you new?{' '}
+            <a href='#' className='text-blue-400'>
+              Sign up
+            </a>
+          </p>
         </div>
-
-        <p className="mt-3 text-gray-800">
-          {isLogin ? 'Are you new? ' : 'Do you have an account? '}
-          <a href="#" onClick={toogleLogin} className="text-blue-400">
-            {isLogin ? 'Sign up' : 'Sign in'}
-          </a>
-        </p>
-
         <LoginForm />
 
-        <p className="text-sm mt-12 text-center">
+        <p className='text-sm mt-12 text-center'>
           Created by{' '}
-          <a href="https://github.com/jorgelhd94" target="_blank">
+          <a href='https://github.com/jorgelhd94' target='_blank'>
             JCode Studio
           </a>
         </p>
