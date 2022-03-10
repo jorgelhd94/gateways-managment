ca973444788fb8a4ff508cbcce231a0187b12cda
Jorgito <jorgelhd94@gmail.com>
2022-03-10 11:29:05 -0500

    add yup schema

--- src/Components/Auth/Forms/RegisterForm/RegisterForm.jsx
+++ src/Components/Auth/Forms/RegisterForm/RegisterForm.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEnvelope, faLock, faCheckDouble } from '@fortawesome/free-solid-svg-icons';
+import { faPerson, faEnvelope, faLock, faCheckDouble } from '@fortawesome/free-solid-svg-icons';
 import { faGoogle } from '@fortawesome/free-brands-svg-icons';
+import * as yup from 'yup';
 
 const RegisterForm = () => {
+  const schema = yup.object({
+    name: yup.string().required().min(3).max(50),
+    email: yup.string().required().email(),
+    password: yup.string().required().min(6).max(32),
+    repeat: yup
+      .string()
+      .required()
+      .oneOf([yup.ref('password'), null], 'Passwords must match')
+  });
+
   const handleOnSubmit = (e) => {
     e.preventDefault();
   };
@@ -11,6 +22,22 @@ const RegisterForm = () => {
   return (
     <form onSubmit={handleOnSubmit}>
       <div className="mt-12">
+        
+        <div className="my-6">
+          <div className="flex relative ">
+            <span className="rounded-l-md inline-flex  items-center px-3 border-t bg-white border-l border-b  border-gray-300 text-gray-500 shadow-sm text-sm">
+              <FontAwesomeIcon icon={faPerson} />
+            </span>
+            <input
+              type="text"
+              id="name-with-icon"
+              className=" rounded-r-lg flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent"
+              name="name"
+              placeholder="Your name"
+            />
+          </div>
+        </div>
+        
         <div className="my-6">
           <div className="flex relative ">
             <span className="rounded-l-md inline-flex  items-center px-3 border-t bg-white border-l border-b  border-gray-300 text-gray-500 shadow-sm text-sm">
