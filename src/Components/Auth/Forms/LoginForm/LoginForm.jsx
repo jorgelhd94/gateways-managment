724e3a474fdfefcac269755ae37f02acb326afa7
Jorgito <jorgelhd94@gmail.com>
2022-03-10 15:49:09 -0500

    fixing Login Style

--- src/Components/Auth/Forms/LoginForm/LoginForm.jsx
+++ src/Components/Auth/Forms/LoginForm/LoginForm.jsx
@@ -2,70 +2,79 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEnvelope, faLock } from '@fortawesome/free-solid-svg-icons';
 import { faGoogle } from '@fortawesome/free-brands-svg-icons';
+import { Formik, Field, Form, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+import FieldInput from '../../../UI/IconInput/IconInput';
+
+import {
+  successInputClass,
+  errorInputClass
+} from '../../../../utils/inputStyle';
 
 const LoginForm = () => {
-  const handleOnSubmit = (e) => {
-      e.preventDefault();
+  const requierdMsg = 'This is a required field';
+
+  const schema = Yup.object({
+    email: Yup.string().required(requierdMsg).email(),
+    password: Yup.string().required(requierdMsg).min(6).max(32)
+  });
+
+  const getError = (inputName) => {
+    return <ErrorMessage name={inputName} />;
   };
 
   return (
-    <form onSubmit={handleOnSubmit}>
-      <div className='mt-12'>
-        
-        <div className='my-6'>
-          <div className='flex relative '>
-            <span className='rounded-l-md inline-flex  items-center px-3 border-t bg-white border-l border-b  border-gray-300 text-gray-500 shadow-sm text-sm'>
-              <FontAwesomeIcon icon={faEnvelope} />
-            </span>
-            <input
-              type='text'
-              id='email-with-icon'
-              className=' rounded-r-lg flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent'
-              name='email'
-              placeholder='Your email'
-            />
-          </div>
-        </div>
+    <div className="mt-12">
+      <Formik
+        initialValues={{ email: '', password: '' }}
+        validationSchema={schema}
+        onSubmit={(values, { setSubmitting }) => {
+          setTimeout(() => {
+            alert(JSON.stringify(values, null, 2));
+            setSubmitting(false);
+          }, 400);
+        }}>
+        {({ errors }) => (
+          <Form>
+            <FieldInput icon={faEnvelope} error={getError('email')}>
+              <Field
+                name="email"
+                type="email"
+                className={errors.email ? errorInputClass : successInputClass}
+                placeholder="Your email"
+              />
+            </FieldInput>
 
-        <div className='my-6'>
-          <div className='flex relative '>
-            <span className='rounded-l-md inline-flex  items-center px-3 border-t bg-white border-l border-b  border-gray-300 text-gray-500 shadow-sm text-sm'>
-              <FontAwesomeIcon icon={faLock} />
-            </span>
-            <input
-              type='text'
-              id='password-with-icon'
-              className=' rounded-r-lg flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent'
-              name='password'
-              placeholder='Your password'
-            />
-          </div>
-        </div>
-        
-        <div className='my-6'>
-          <button
-            className='inline-block rounded-sm font-medium border border-solid cursor-pointer text-center text-base py-3 px-6 text-white bg-blue-400 border-blue-400 hover:bg-blue-600 hover:border-blue-600 w-full'
-            type='submit'
-          >
-            Log In
+            <FieldInput icon={faLock} error={getError('password')}>
+              <Field
+                name="password"
+                type="password"
+                className={errors.password ? errorInputClass : successInputClass}
+                placeholder="Your password"
+              />
+            </FieldInput>
+
+            <div className="my-6">
+              <button
+                className="inline-block rounded-sm font-medium border border-solid cursor-pointer text-center text-base py-3 px-6 text-white bg-blue-400 border-blue-400 hover:bg-blue-600 hover:border-blue-600 w-full"
+                type="submit">
+                Sign in
+              </button>
+            </div>
+          </Form>
+        )}
+      </Formik>
+
+      <div className="flex flex-col mt-4 justify-items-center">
+        <p className="text-gray-800 mb-4">Or</p>
+        <div className="flex justify-center">
+          <button className="flex items-center px-6 py-2 transition ease-in duration-200 uppercase rounded-full hover:bg-gray-800 hover:text-white border-2 border-gray-900 focus:outline-none">
+            <FontAwesomeIcon icon={faGoogle} className="mr-4" />
+            Sign in with Google
           </button>
         </div>
-        <div className='text-right'>
-          <a href='#' className='text-blue-400'>
-            Forgot your password?
-          </a>
-        </div>
-        <div className='flex flex-col mt-4 justify-items-center'>
-          <p className='text-gray-800 mb-4'>Or</p>
-          <div className='flex justify-center'>
-            <button className='flex items-center px-6 py-2 transition ease-in duration-200 uppercase rounded-full hover:bg-gray-800 hover:text-white border-2 border-gray-900 focus:outline-none'>
-              <FontAwesomeIcon icon={faGoogle} className='mr-4' />
-              Sign in with Google
-            </button>
-          </div>
-        </div>
       </div>
-    </form>
+    </div>
   );
 };
 
--- src/Components/Auth/Forms/RegisterForm/RegisterForm.jsx
+++ src/Components/Auth/Forms/RegisterForm/RegisterForm.jsx
@@ -6,15 +6,12 @@ import { Formik, Field, Form, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 import FieldInput from '../../../UI/IconInput/IconInput';
 
+import {
+  successInputClass,
+  errorInputClass
+} from '../../../../utils/inputStyle';
+
 const RegisterForm = () => {
-  const genericInputClass =
-    'rounded-r-lg flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base';
-  const errorInputClass =
-    genericInputClass +
-    ' ring-2 ring-red-600 focus:outline-none focus:ring-2 focus:ring-red-600 focus:border-transparent';
-  const successInputClass =
-    genericInputClass +
-    ' focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent';
 
   const requierdMsg = 'This is a required field';
 
