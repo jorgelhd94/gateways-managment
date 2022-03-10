74a1ee9b31f3ef0f2b4309ffa82643ffae373c56
Jorgito <jorgelhd94@gmail.com>
2022-03-10 15:58:57 -0500

    add Router

--- src/Components/Auth/Forms/LoginForm/LoginForm.jsx
+++ src/Components/Auth/Forms/LoginForm/LoginForm.jsx
@@ -32,7 +32,7 @@ const LoginForm = () => {
           setTimeout(() => {
             alert(JSON.stringify(values, null, 2));
             setSubmitting(false);
-          }, 400);
+          }, 1000);
         }}>
         {({ errors }) => (
           <Form>
