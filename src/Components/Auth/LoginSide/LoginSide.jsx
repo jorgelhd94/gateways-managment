4f08b8e2489ddfe0192407d95fcec2902c98f862
Jorgito <jorgelhd94@gmail.com>
2022-03-11 08:43:06 -0500

    fix img login

--- src/Components/Auth/LoginSide/LoginSide.jsx
+++ src/Components/Auth/LoginSide/LoginSide.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import Image from 'next/image';
+import loginImg from '../../../../public/assets/img/bg-login.jpg';
 import LoginForm from '../Forms/LoginForm/LoginForm';
 import RegisterForm from '../Forms/RegisterForm/RegisterForm';
 
@@ -10,32 +12,34 @@ const LoginSide = () => {
   };
 
   return (
-    <div className='flex min-h-screen'>
-      <div className="hidden lg:block w-5/12">
-        <img src="src/assets/img/bg-login.jpg" className="w-full h-full object-cover" />
+    <div className="absolute w-screen h-screen flex">
+      <div className="hidden lg:block w-4/12 h-screen overflow-hidden">
+        <Image src={loginImg} alt="Welcome Picture" objectFit="cover" className="w-full h-full object-cover" />
       </div>
 
-      <div className="w-5/6 sm:w-1/2 mx-auto text-center lg:px-8 xl:px-40 mt-16">
-        <div className="mt-4">
-          <h2 className="text-3xl font-bold text-gray-800">
-            {isLogin ? 'Welcome' : 'Register'} to GMS!!
-          </h2>
-          <p className="mt-3 text-gray-800">
-            {isLogin ? 'Are you new?' : 'Do you have an account?'}{' '}
-            <a href="#" className="text-blue-400" onClick={toogleLogin}>
-              {isLogin ? 'Sign up' : 'Sign In'}
+      <div className='w-full lg:w-8/12 overflow-scroll overflow-x-hidden py-16 relative'>
+        <div className="w-5/6 sm:w-1/2 mx-auto text-center lg:px-8">
+          <div className="mt-4">
+            <h2 className="text-3xl font-bold text-gray-800">
+              {isLogin ? 'Welcome' : 'Register'} to GMS!!
+            </h2>
+            <p className="mt-3 text-gray-800">
+              {isLogin ? 'Are you new?' : 'Do you have an account?'}{' '}
+              <a href="#" className="text-blue-400" onClick={toogleLogin}>
+                {isLogin ? 'Sign up' : 'Sign In'}
+              </a>
+            </p>
+          </div>
+
+          {isLogin ? <LoginForm /> : <RegisterForm />}
+
+          <p className="text-sm my-12 text-center">
+            Created by{' '}
+            <a href="https://github.com/jorgelhd94" target="_blank" rel="noreferrer">
+              JCode Studio
             </a>
           </p>
         </div>
-
-        {isLogin ? <LoginForm /> : <RegisterForm />}
-
-        <p className="text-sm my-12 text-center">
-          Created by{' '}
-          <a href="https://github.com/jorgelhd94" target="_blank" rel="noreferrer">
-            JCode Studio
-          </a>
-        </p>
       </div>
     </div>
   );
