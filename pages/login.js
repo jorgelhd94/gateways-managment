617f0a3fc484f633754db0ad18d6b65afe300084
Jorgito <jorgelhd94@gmail.com>
2022-03-11 07:56:06 -0500

    order files

---
+++ pages/login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Login from '../Components/Auth/LoginSide/LoginSide';
+
+const Auth = () => {
+  return (
+    <div>
+      <Login></Login>
+    </div>
+  );
+};
+
+export default Auth;
