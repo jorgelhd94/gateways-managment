c9b448bc696a6df21975d59204595c96234438ca
Jorgito <jorgelhd94@gmail.com>
2022-03-11 15:09:00 -0500

    adding disabled button while submitting

--- src/Components/Auth/Forms/RegisterForm/RegisterForm.jsx
+++ src/Components/Auth/Forms/RegisterForm/RegisterForm.jsx
@@ -96,7 +96,7 @@ const RegisterForm = () => {
             <div className="my-6">
               <button
                 className="inline-block rounded-sm font-medium border border-solid cursor-pointer text-center text-base py-3 px-6 text-white bg-blue-400 border-blue-400 hover:bg-blue-600 hover:border-blue-600 w-full"
-                type="submit">
+                type="submit" disabled={isLoading}>
                 {isLoading && <FontAwesomeIcon icon={faCircleNotch} className="mr-2 fa-spin" />}
                 Register
               </button>
