1483985be099cdc5e38aada6e8d9e44d1d87f831
Jorgito <jorgelhd94@gmail.com>
2022-03-11 10:51:51 -0500

    config functions firebase

--- src/includes/firebase.js
+++ src/includes/firebase.js
@@ -1,21 +1,24 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+import { getFunctions, httpsCallable, connectFunctionsEmulator } from 'firebase/functions';
+
 const firebaseConfig = {
-  apiKey: "AIzaSyCNhRRq3iDxLPFOt01Zy-IKCEGiKAfRYrU",
-  authDomain: "gateways-managment.firebaseapp.com",
-  projectId: "gateways-managment",
-  storageBucket: "gateways-managment.appspot.com",
-  messagingSenderId: "739831649052",
-  appId: "1:739831649052:web:f657c1559cf6e0fa09a0d9",
-  measurementId: "G-T46V1Q283K"
+  apiKey: 'AIzaSyCNhRRq3iDxLPFOt01Zy-IKCEGiKAfRYrU',
+  authDomain: 'gateways-managment.firebaseapp.com',
+  projectId: 'gateways-managment',
+  storageBucket: 'gateways-managment.appspot.com',
+  messagingSenderId: '739831649052',
+  appId: '1:739831649052:web:f657c1559cf6e0fa09a0d9',
+  measurementId: 'G-T46V1Q283K'
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+
+const functions = getFunctions(app);
+connectFunctionsEmulator(functions, 'localhost', 5001);
+
+export { functions, httpsCallable };
