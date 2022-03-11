889e390dc6addae7e2b9a3ec3dce19b1cd36f970
Jorgito <jorgelhd94@gmail.com>
2022-03-11 14:28:14 -0500

    Revert "organize functions"
    This reverts commit 81c2897a3fc2ed2bc1fdd0cb417453ff79ba7bee.

--- functions/index.js
+++ functions/index.js
@@ -1,17 +1,10 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-
-const { routesConfig } = require('./controllers/users/routes-config');
-
-admin.initializeApp();
-
-const app = express();
-app.use(bodyParser.json());
-app.use(cors({ origin: true }));
-routesConfig(app);
-
-exports.api = functions.https.onRequest(app);
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+exports.login = functions.https.onRequest((request, response) => {
+  functions.logger.info("Hello logs!", {structuredData: true});
+  response.json({result: "Hello MTF!"});
+});
