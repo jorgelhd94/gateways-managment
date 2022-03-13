3615c32d8561cbb4c7389dec4fc4012a82e90038
Jorgito <jorgelhd94@gmail.com>
2022-03-13 17:20:42 -0400

    fix google button

--- src/Components/UI/Buttons/ButtonGoogle/ButtonGoogle.jsx
+++ src/Components/UI/Buttons/ButtonGoogle/ButtonGoogle.jsx
@@ -15,12 +15,6 @@ const ButtonGoogle = (props) => {
 
     await signInWithPopup(auth, provider)
       .then((result) => {
-        // This gives you a Google Access Token. You can use it to access the Google API.
-        // const credential = GoogleAuthProvider.credentialFromResult(result);
-        // const token = credential.accessToken;
-        // The signed-in user info.
-        // const user = result.user;
-
         toast.success('Welcome!!');
         router.push('/');
       })
