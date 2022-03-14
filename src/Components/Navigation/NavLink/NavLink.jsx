bf91e3c7987b4abd023aecea63ffaf2894ed6d15
Jorgito <jorgelhd94@gmail.com>
2022-03-14 16:59:06 -0400

    finish link

--- src/Components/Navigation/NavLink/NavLink.jsx
+++ src/Components/Navigation/NavLink/NavLink.jsx
@@ -4,15 +4,19 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const NavLink = (props) => {
   const defaultClass =
-    'w-full text-gray-800 dark:text-white flex items-center pl-6 p-2 my-2 transition-colors duration-200 justify-start';
-  const activeClass = defaultClass + ' border-l-4 border-blue-500';
-  
+    'w-full flex items-center pl-6 p-2 my-2 transition-colors duration-200 justify-start';
+  const normalClass =
+    defaultClass + ' text-gray-400 hover:text-gray-800 border-l-4 border-transparent';
+  const activeClass = defaultClass + ' text-gray-800 dark:text-white border-l-4 border-blue-500';
+
+  const textClass = 'text-sm font-normal';
+
   return (
-    <a className={props.active ? defaultClass : activeClass} href="#">
+    <a className={!props.active ? normalClass : activeClass} href="#">
       <span className="text-left">
         <FontAwesomeIcon icon={props.icon} />
       </span>
-      <span className="mx-2 text-sm font-normal">{props.name}</span>
+      <span className={props.active ? textClass + ' mx-2' : textClass + ' mx-4'}>{props.name}</span>
     </a>
   );
 };
