b94ad0f4f5d44ab3b08fd7d3023d47a452f81090
Jorgito <jorgelhd94@gmail.com>
2022-03-15 08:45:47 -0400

    fixing Card

--- pages/gateways/index.js
+++ pages/gateways/index.js
@@ -1,8 +1,11 @@
-import React from 'react'
-import Dashboard from '../../src/components/Layout/Dashboard/Dashboard'
+import React from 'react';
+import Dashboard from '../../src/components/Layout/Dashboard/Dashboard';
+import Card from '../../src/components/UI/Card/Card';
 
 export default function Gateways() {
   return (
-    <Dashboard>Gateways Page</Dashboard>
-  )
+    <Dashboard>
+      <Card>Gateways Page</Card>
+    </Dashboard>
+  );
 }
--- src/Components/UI/Card/Card.jsx
+++ src/Components/UI/Card/Card.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Card = ({ children }) => {
   return (
-    <div className="flex flex-col w-full max-w-md px-4 py-8 bg-white rounded-lg shadow dark:bg-gray-800 sm:px-6 md:px-8 lg:px-10">
+    <div className="flex flex-col m-4 px-4 py-8 bg-white rounded-lg shadow dark:bg-gray-800 sm:px-6 md:px-8 lg:px-10">
       {children}
     </div>
   );
