853dd3bcc42ca1ff40fe11789f06fca96d32f22a
Jorgito <jorgelhd94@gmail.com>
2022-03-15 14:25:18 -0400

    create a new gateway

--- functions/controllers/gateway.js
+++ functions/controllers/gateway.js
@@ -1,8 +1,15 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 
-exports.create = functions.https.onCall(async (data, context) => {  
-  return {
-    text: 'OK'
-  };
+exports.create = functions.https.onCall(async (data, context) => {
+  await admin
+    .firestore()
+    .collection('gateway')
+    .add({ ...data })
+    .then((response) => {
+      return  response;
+    })
+    .catch((error) => {
+      throw new functions.https.HttpsError('aborted', error);
+    });
 });
