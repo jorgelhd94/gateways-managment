28e65956655d9cb4aabf54c0f41fa1f428fde34c
Jorgito <jorgelhd94@gmail.com>
2022-03-17 08:42:40 -0400

    fix Skeleton style

--- src/Components/UI/Skeleton/FormSkeleton/FormSkeleton.jsx
+++ src/Components/UI/Skeleton/FormSkeleton/FormSkeleton.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const FormSkeleton = () => {
   return (
-    <div className="bg-white rounded w-full mx-auto rounded-2xl mt-8">
+    <div className="bg-white w-full mx-auto rounded-2xl mt-8">
       <div className="">
         <div className="grid grid-cols-3 gap-4 mt-2">
           <div className="h-16 bg-gray-200 rounded animate-pulse"></div>
