fdbcc300d388af31c70d8b0dd7489a4678a7114a
Jorgito <jorgelhd94@gmail.com>
2022-03-17 16:47:09 -0400

    add custom type to badge

--- src/Components/UI/Badge/Badge.jsx
+++ src/Components/UI/Badge/Badge.jsx
@@ -1,12 +1,25 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Badge = ({children}) => {
+const Badge = ({ children, type }) => {
+  const styles = {
+    primary: 'bg-blue-500',
+    success: 'bg-green-500',
+    danger: 'bg-red-500',
+    sad: 'bg-gray-500'
+  };
+  const defaultClass = 'px-2 py-1 text-white rounded-full ';
+  const typeClass = type ? defaultClass + styles[type] : defaultClass + styles['primary'];
+
   return (
     <>
-      <span className="px-2 py-1 bg-blue-500 text-white rounded-full">{children}</span>
+      <span className={typeClass}>{children}</span>
     </>
   );
 };
 
+Badge.propTypes = {
+  type: PropTypes.oneOf(['primary', 'success', 'danger', 'sad'])
+};
 
 export default Badge;
