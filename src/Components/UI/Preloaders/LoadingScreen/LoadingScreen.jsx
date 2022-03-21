02f04ad575e66d27727d0d2484427e184b2d5832
Jorgito <jorgelhd94@gmail.com>
2022-03-21 09:30:42 -0400

    add priority to preloader image again

--- src/Components/UI/Preloaders/LoadingScreen/LoadingScreen.jsx
+++ src/Components/UI/Preloaders/LoadingScreen/LoadingScreen.jsx
@@ -6,7 +6,7 @@ const LoadingScreen = () => {
   return (
     <div className={styles.Loader}>
       <div className="flex justify-center h-screen w-screen">
-        <Image src="/assets/img/cube.svg" width={60} height={60}></Image>
+        <Image src="/assets/img/cube.svg" width={60} height={60} priority />
       </div>
     </div>
   );
